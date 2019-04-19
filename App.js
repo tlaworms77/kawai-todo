518a9a4e0ceed39cd51dd6280056bf7be9a25d54
tlaworms77 <tlaworms77@naver.com>
2019-04-19 14:30:08 +0900

    App.json

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  StatusBar, 
+  TextInput, 
+  Dimensions,
+  Platform 
+} from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"} />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,40 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#F23657',
+    alignItems: 'center'
+    // justifyContent: 'center' // 원치 않는다 위로 올라가야하기 때문이다.
   },
+  title: {
+    color: "white", // fontColor
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "rgb(50, 50, 50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1, // 높이는 0으로 설정. 위 아래로 움직이는게 싫기 때문 그래서 -1로 설정.
+          width: 0
+        }
+      },
+      android: {
+        elevation: 5
+      }
+    })
+
+  },
+  input: {
+
+  }
 });
