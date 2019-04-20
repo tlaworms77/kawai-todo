af373b80929d9a42ef3d53f91cf66633c0259210
tlaworms77 <tlaworms77@naver.com>
2019-04-20 17:04:48 +0900

    App Loading setting..

--- App.js
+++ App.js
@@ -9,17 +9,28 @@ import {
   Platform,
   ScrollView
 } from 'react-native';
+import { AppLoading } from 'expo';
 import ToDo from './ToDo';
 
 const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   state = {
-    newToDo: ""
+    newToDo: "",
+    loadedToDos: false
   };
 
+  componentDidMount = () => {
+    this._loadToDos();
+  }
+
   render() {
-    const { newToDo } = this.state;
+    const { newToDo, loadedToDos } = this.state;
+
+    if(!loadedToDos) {
+      return <AppLoading />
+    }
+
     return (
       <View style={styles.container}>
         <StatusBar barStyle="light-content" />
@@ -41,6 +52,13 @@ export default class App extends React.Component {
       </View>
     );
   }
+
+  _loadToDos = () => {
+    this.setState({
+      loadedToDos: true
+    });
+  }
+
   _controlNewToDo = text => {
     this.setState({
       newToDo: text
